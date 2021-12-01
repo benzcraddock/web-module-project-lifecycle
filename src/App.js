ecb48e075f7ee34c3e4a9bf4d8aab4eb8c054abf
Benjamin Craddock <benjamincraddock@benjamins-mbp.lan>
2021-11-30 20:17:39 -0500

    completed user card

--- src/App.js
+++ src/App.js
@@ -1,6 +1,37 @@
 import React from 'react';
+import User from './components/User';
+import FollowerList from './components/FollowerList';
 
 class App extends React.Component {
+  state = {
+    currentUser: "benzcraddock",
+    user: {
+      avatar_url: "https://avatars.githubusercontent.com/u/84107471?v=4",
+      html_url: "https://github.com/benzcraddock",
+      name: "Benjamin Craddock",
+      login: "benzcraddock",
+      public_repos: 50,
+      followers: 29
+    },
+    followers: [
+      {
+        login: "mayankmmmx",
+        avatar_url: "https://avatars.githubusercontent.com/u/5217785?v=4",
+        html_url: "https://github.com/mayankmmmx"
+      },
+      {
+        login: "mayankmmmx",
+        avatar_url: "https://avatars.githubusercontent.com/u/5217785?v=4",
+        html_url: "https://github.com/mayankmmmx"
+      },
+      {
+        login: "mayankmmmx",
+        avatar_url: "https://avatars.githubusercontent.com/u/5217785?v=4",
+        html_url: "https://github.com/mayankmmmx"
+      }
+    ]
+  }
+
   render() {
     return(
     <div>
@@ -10,32 +41,8 @@ class App extends React.Component {
         <button>Search</button>
       </form>
 
-      <div id="UserCard">
-        <img src="https://avatars.githubusercontent.com/u/84107471?v=4"/>
-        <a href="https://github.com/benzcraddock" target="_blank"><h3>Benjamin Craddock</h3></a>
-        <p>(benzcraddock)</p>
-        <p>Total Repos: 50</p>
-        <p>Total Followers: 29</p>
-      </div>
-
-      <div id="followers">
-        <div className="follower">
-          <img width="200px" src="https://avatars.githubusercontent.com/u/5217785?v=4"/>
-          <a href="https://github.com/mayankmmmx" target="_blank"><p>mayankmmmx</p></a>
-        </div>
-        <div className="follower">
-          <img width="200px" src="https://avatars.githubusercontent.com/u/45055471?v=4"/>
-          <a href="https://github.com/Jmz0127" target="_blank"><p>Jmz0127</p></a>
-        </div>
-        <div className="follower">
-          <img width="200px" src="https://avatars.githubusercontent.com/u/60374084?v=4"/>
-          <a href="https://github.com/jmerz826" target="_blank"><p>jmerz826</p></a>
-        </div>
-        <div className="follower">
-          <img width="200px" src="https://avatars.githubusercontent.com/u/68617120?v=4"/>
-          <a href="https://github.com/oscy7" target="_blank"><p>oscy7</p></a>
-        </div>
-      </div>
+      <User user={this.state.user}/>
+      <FollowerList followers={this.state.followers}/>
       
     </div>
     );
---
+++ src/components/Follower.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+class Follower extends React.Component {
+  render () {
+    return(
+      <div className="follower">
+        <img width="200px" src="https://avatars.githubusercontent.com/u/5217785?v=4"/>
+        <a href="https://github.com/mayankmmmx" target="_blank"><p>mayankmmmx</p></a>
+      </div>
+      // <div className="follower">
+      //   <img width="200px" src="https://avatars.githubusercontent.com/u/45055471?v=4"/>
+      //   <a href="https://github.com/Jmz0127" target="_blank"><p>Jmz0127</p></a>
+      // </div>
+      // <div className="follower">
+      //   <img width="200px" src="https://avatars.githubusercontent.com/u/60374084?v=4"/>
+      //   <a href="https://github.com/jmerz826" target="_blank"><p>jmerz826</p></a>
+      // </div>
+      // <div className="follower">
+      //   <img width="200px" src="https://avatars.githubusercontent.com/u/68617120?v=4"/>
+      //   <a href="https://github.com/oscy7" target="_blank"><p>oscy7</p></a>
+      // </div>
+    )
+  }
+}
+
+export default Follower;
